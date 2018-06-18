1cb2637dc24c8433e87a5660e1a0beeab01c36c5
Calvin Ageneau <calvinageneau@hotmail.fr>
2018-06-18 14:13:13 +0200

    u

--- index.js
+++ index.js
@@ -140,7 +140,8 @@ client.on('message', function (message) {
     const emb = new Discord.RichEmbed()
     .setTitle("Liste des musiques jouées");
     for (var i = 0; i < queueNames.length; i++) {
-      emb.addField("" + (i + 1),queueNames[i] + (i === 0 ? " **(Musique actuelle)**" : ""));
+      if(i === 0) emb.addField((i + 1) + queueNames[i] + " **(Musique actuelle)**");
+      else emb.addField((i + 1) + queueNames[i] + "");
     }
     message.reply({emb});
     console.log({emb});
